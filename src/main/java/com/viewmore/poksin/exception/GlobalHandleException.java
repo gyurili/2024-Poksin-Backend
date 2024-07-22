683f50590dd7a0d99a2212f91c6bf414d0747667
7beunseo <kimes0403@gmail.com>
2024-07-22 13:59:31 +0900

    [FEAT] 회원가입 예외 처리 #1

---
+++ src/main/java/com/viewmore/poksin/exception/GlobalHandleException.java
@@ -0,0 +1,17 @@
+package com.viewmore.poksin.exception;
+
+import com.viewmore.poksin.code.ErrorCode;
+import com.viewmore.poksin.response.ErrorResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalHandleException {
+    @ExceptionHandler(DuplicateUsernameException.class)
+    protected ResponseEntity<ErrorResponseDTO> handleDuplicateUsernameException(final DuplicateUsernameException e) {
+        return ResponseEntity
+                .status(ErrorCode.DUPLICATE_USERNAME.getStatus().value())
+                .body(new ErrorResponseDTO(ErrorCode.DUPLICATE_USERNAME));
+    }
+}
