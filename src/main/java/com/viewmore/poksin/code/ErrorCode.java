f4e46c1cd529ecab1db7e55d6dda91591cabc005
7beunseo <kimes0403@gmail.com>
2024-07-22 16:09:07 +0900

    [FEAT] 토큰 관련 성공, 실패 응답 추가 #3

--- src/main/java/com/viewmore/poksin/code/ErrorCode.java
+++ src/main/java/com/viewmore/poksin/code/ErrorCode.java
@@ -8,7 +8,24 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     /**
-     * 409 CONFLICT: 중복된 이메일
+     * 400
+     */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    /**
+     * 401
+     */
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
+    TOKEN_MISSING(HttpStatus.UNAUTHORIZED, "요청 헤더에 토큰이 없습니다."),
+
+    /**
+     * 404
+     */
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+
+    /**
+     * 409
      */
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "중복된 유저 이름입니다."),
     ;
--- src/main/java/com/viewmore/poksin/code/SuccessCode.java
+++ src/main/java/com/viewmore/poksin/code/SuccessCode.java
@@ -7,7 +7,15 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum SuccessCode {
+    /**
+     * 회원가입
+     */
     SUCCESS_REGISTER(HttpStatus.OK, "회원가입을 성공했습니다."),
+
+    /**
+     * 로그인
+     */
+    SUCCESS_LOGIN(HttpStatus.OK, "로그인을 성공했습니다. 헤더 토큰을 확인하세요."),
 
     ;
 
